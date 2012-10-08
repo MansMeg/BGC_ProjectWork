dbe24dc7e57b63d30e6fc4fa97ffc0e522f4ddb1
Måns Magnusson <mons.magnusson@gmail.com>
2012-10-08 20:42:29 +0200

    GSE version 0.3
    Lagt till simulationer och plottar av intresse.

--- BCG Report GSE.R
+++ BCG Report GSE.R
@@ -3,133 +3,168 @@
 # Modelling an influenza vaccination intervention using 
 # different modelling strategies
 
-
-
 # General stoschastic epidemic
 # Paramaters
-set.seed(20121006)
-n.c<-1
+seed<-1917+0:2
 n.all<-c(200,1000,5000)
-n<-n.all[n.c] # Population
-n.c<-n.c+1
 
-m<-5 # Initially infected
+
+m<-1 # Initially infected
 lambda <- 0.5 # Lambda of Poisson process (no of contacts)
 gamma <- 4.8 # Gamma of Exp model of infection time
-# vacc.ant <- 0 # Number of vaccinated each time point 
- vacc.ant <- round(n*0.1) # Number of vaccinated each time point 
+# Number of vaccinated each time point
+vacc.ant <- matrix(c(rep(0,3),rep(round(n.all*0.05),2)),nrow=3) 
+vacc.start.T <- c(0,0,10)
+vacc.int.T <- rep(1,3)
 
-vacc.start.T <- 10
-vacc.int.T <- 2
-
-# Create data
-sim.data<-data.frame(inf=rep(0,n),
-                     rec=rep(0,n),
-                     inf.t=rep(NA,n),
-                     inf.length=rep(NA,n),
-                     contacts=rep(NA,n),
-                     rec.t=rep(NA,n))
-
-# Adding index case(s)
-index.no<-sample(x=1:n,size=m)
-sim.data[index.no,]<-matrix(data=rep(c(1,0,0,NA,NA,NA),m),nrow=m,byrow=T)
-
-# Counting variables
-t.int<-vacc.start.T
-vacc.nr<-1
-
-# Simulation
-while(sum(sim.data$inf>sim.data$rec)>0){
-  # Identifies the next infectious person
-  inf.no<-which(sim.data$inf.t==min(sim.data$inf.t[sim.data$inf==1 & sim.data$rec==0],na.rm=T))
-  if(length(inf.no)==0){break}
-    
-  # Vaccination
-  if(min(sim.data[inf.no,3])>t.int && vacc.ant>0){
-  	to.vacc<-((vacc.nr-1)*vacc.ant+1):(vacc.nr*vacc.ant)
-  	
-  	sim.data[to.vacc,2]<-1
-
- 	 	# Infected after vaccination
-  		after.vac<-sim.data[to.vacc,1]==1 & sim.data[to.vacc,3]>t.int
-  		sim.data[to.vacc[after.vac],1]<-0
-  		sim.data[to.vacc[after.vac],3]<-NA
- 
-  	print(paste("Vaccinated",vacc.ant,"persons at t =",t.int,sep=" "))
-  	t.int<-t.int+vacc.int.T
-  	vacc.nr<-vacc.nr+1
- 	  	
-  }
-
-  # Identifies the next infectious person
-  inf.no<-which(sim.data$inf.t==min(sim.data$inf.t[sim.data$inf==1 & sim.data$rec==0],na.rm=T))
-  if(length(inf.no)==0){break}
-  
-  # Simulates infection time and no of contacts
-  sim.data[inf.no,4]<-rexp(length(inf.no),rate=1/gamma)
-  sim.data[inf.no,5]<-rpois(length(inf.no),lambda*sim.data[inf.no,4])
-  
-  # Categorizing the infectious persons as "recovered"
-  sim.data[inf.no,2]<-1
-  
-  # Randomizing new infectious persons
-  for(j in 1:length(inf.no)){
-    if(sim.data[inf.no[j],5]>0){
-      new.inf<-ceiling(n*runif(sim.data[inf.no[j],5]))
-      new.inf<-new.inf[sim.data[new.inf,2]==0] # Only selecting "new" infections
-      
-      # Simulating infection times for newly infected
-      if(length(new.inf)>0){
-        sim.data[new.inf,1]<-1
-        
-        inf.t<-runif(length(new.inf),
-                   min=sim.data[inf.no[j],3],
-                   max=sim.data[inf.no[j],3]+sim.data[inf.no[j],4])
-        sim.data[new.inf,3]<-pmin(sim.data[new.inf,3],inf.t,na.rm=T)
-      }
-    }  
+sim.res<-list();i<-1;j<-1
+for (j in 1:3){
+  for (i in 1:3){
+    print(c(i,j))
+    sim.res<-c(sim.res,
+               list(
+                 GSE.sim(n=n.all[i],
+                         m=m,lambda=lambda,gamma=gamma,
+                         vacc.ant=vacc.ant[i,j],
+                         vacc.start.T=vacc.start.T[j],
+                         vacc.int.T=vacc.int.T[j],
+                         seed=seed[i])[[2]]))
   }
 }
 
-# Calculates the recovery time
-inf<-!is.na(sim.data$inf.t)
-#sim.data$rec.t[inf]<-ifelse(sim.data$inf.t[inf]+sim.data$inf.length[inf]>sim.data$rec.t[inf],sim.data$rec.t[inf],sim.data$inf.t[inf]+sim.data$inf.length[inf])
-sim.data$rec.t<-sim.data$inf.t+sim.data$inf.length
-
-
-#sim.data$inf.t[sim.data$inf.length==0]<-NA
-#sim.data$inf.length[sim.data$inf.length==0]<-NA
-
-# Summarizing the data based on time at rec and inf
-inf.df<-as.data.frame(aggregate(sim.data$inf.t,by=list(sim.data$inf.t),FUN=length))
-names(inf.df)<-c("time","new.inf")
-
-rec.df<-as.data.frame(aggregate(sim.data$rec.t,by=list(sim.data$rec.t),FUN=length))
-names(rec.df)<-c("time","new.rec")
-
-sim.data.aggr<-merge(x=inf.df,y=rec.df,by="time",all=T)
-sim.data.aggr[is.na(sim.data.aggr)]<-0
-
-# Calculating S(t), I(t) and R(t)
-sim.data.aggr$S<-n-cumsum(sim.data.aggr$new.inf)
-sim.data.aggr$R<-cumsum(sim.data.aggr$new.rec)
-sim.data.aggr$I<-cumsum(sim.data.aggr$new.inf)-sim.data.aggr$R
-sim.data.aggr$V<-0
-for(i in 1:(vacc.nr-1)){
-	sim.data.aggr$V	[sim.data.aggr$time>vacc.start.T+(i-1)*vacc.int.T]<-i*vacc.ant}
-
-# Save the final results
-if(exists("sim.res")){sim.res<-rbind(sim.res,sim.data.aggr[dim(sim.data.aggr)[1],])
-                      }else{
-                        sim.res<-sim.data.aggr[dim(sim.data.aggr)[1],]
-                      }
+sim.no<-1000
+sim.peak.all<-list();i<-1;j<-1
+for (j in 1:3){
+  sim.peak<-list()
+  for (i in 1:sim.no){
+    print(c(j,i))
+    temp.data<-GSE.sim(n=n.all[2],
+                         m=m,lambda=lambda,gamma=gamma,
+                         vacc.ant=vacc.ant[2,j],
+                         vacc.start.T=vacc.start.T[j],
+                         vacc.int.T=vacc.int.T[j],
+                         seed=seed[2]+i)[[2]]
+    max.no<-which(max(temp.data$I)==temp.data$I)[1]
+    temp.data$cum.I<-temp.data$R[dim(temp.data)[1]]
+    temp.data$max.T<-temp.data$time[dim(temp.data)[1]]
+    if(i==1){sim.peak<-temp.data[max.no,]}else{sim.peak<-rbind(sim.peak,temp.data[max.no,])}
+  }
+  if(i==1){sim.peak.all<-list(sim.peak)}else{sim.peak.all<-c(sim.peak.all,list(sim.peak))}
+}
+warnings()
 
 # Plot the results
-if(n.c==2){png(width=550,height=600);par(mfrow=c(3,1))}
-plot(sim.data.aggr$time,sim.data.aggr$S,type="l",col="Blue",ylim=c(0,n),ylab="Population size",xlab="Time")
-lines(x=sim.data.aggr$time,sim.data.aggr$I, col="Red")
-lines(x=sim.data.aggr$time,sim.data.aggr$R, col="Green")
-lines(x=sim.data.aggr$time,sim.data.aggr$V, col="Yellow")
+interv<-c("No vaccine intervention",
+          "Vaccination started at t = 0",
+          "Vaccination started at t = 10")
+
+png(file="PeakT.01.png",width=623,height=600)
+par(mfrow=c(3,1))
+for (k in 1:3){
+  hist(sim.peak.all[[k]]$time,breaks=100,
+       xlim=c(0,max(sim.peak.all[[1]]$time)),
+       main=interv[k],
+       ylab="Simulations",
+       xlab="Peak of epidemic")
+}
+dev.off()
+
+png(file="MaxT.01.png",width=623,height=600)
+par(mfrow=c(3,1))
+for (k in 1:3){
+  hist(sim.peak.all[[k]]$max.T,breaks=100,
+       xlim=c(0,max(sim.peak.all[[1]]$max.T)),
+       main=interv[k],
+       ylab="Simulations",
+       xlab="Length of epidemic")  
+}
+dev.off()
+
+png(file="CumI.01.png",width=623,height=600)
+par(mfrow=c(3,1))
+for (k in 1:3){
+  hist(sim.peak.all[[k]]$cum.I,breaks=100,
+       xlim=c(0,max(sim.peak.all[[1]]$cum.I)),
+       main=interv[k],
+       ylab="Simulations",
+       xlab="Total number of infected")
+  
+}
+dev.off()
+
+png(file="Scatter.01.png",width=623,height=600)
+par(mfrow=c(3,1))
+for (k in 1:3){
+  with(sim.peak.all[[k]],
+       plot(max.T,cum.I,
+            ylim=c(0,max(sim.peak.all[[1]]$cum.I)),
+            xlim=c(0,max(sim.peak.all[[1]]$max.T)),
+            main=interv[k],
+            ylab="Infected",
+            xlab="Length of epidemic")
+  )
+}
+dev.off()
+
+
+# Plot the results
+png(file="DiffPop01.png",width=623,height=600)
+par(mfrow=c(3,1))
+for(k in 1:3){
+  n<-sim.res[[k]]$S[1]+sim.res[[k]]$I[1]+sim.res[[k]]$R[1]
+  plot(sim.res[[k]]$time,sim.res[[k]]$S,type="l",col="Green",ylim=c(0,n),ylab="Population size",xlab="Time")
+  lines(x=sim.res[[k]]$time,sim.res[[k]]$I, col="Red")
+  lines(x=sim.res[[k]]$time,sim.res[[k]]$R, col="Blue")
+#  lines(x=sim.res[[k]]$time,sim.res[[k]]$V, col="Black")
+  legend("topright", c("Susceptibles", "Infecteds", "Recovereds"), 
+         pch = 1, col = c("Green","Red","Blue"))
+}
+dev.off()
+
+png(file="DiffPop02.png",width=623,height=600)
+par(mfrow=c(3,1))
+for(k in c(2,5,8)){
+  n<-sim.res[[k]]$S[1]+sim.res[[k]]$I[1]+sim.res[[k]]$R[1]
+  plot(sim.res[[k]]$time,sim.res[[k]]$S,type="l",col="Green",ylim=c(0,n),ylab="Population size",xlab="Time")
+  lines(x=sim.res[[k]]$time,sim.res[[k]]$I, col="Red")
+  lines(x=sim.res[[k]]$time,sim.res[[k]]$R, col="Blue")
+  lines(x=sim.res[[k]]$time,sim.res[[k]]$V, col="Black")
+  legend("topright", c("Susceptibles", "Infecteds", "Recovereds", "Vaccinated"), 
+         pch = 1, col = c("Green","Red","Blue","Black"))
+}
+dev.off()
+
+png(file="DiffPop12.png",width=623,height=600)
+par(mfrow=c(3,3))
+for(k in 1:3){
+  n<-sim.res[[k]]$S[1]+sim.res[[k]]$I[1]+sim.res[[k]]$R[1]
+  plot(sim.res[[k]]$time,sim.res[[k]]$S,type="l",col="Green",ylim=c(0,n),ylab="Population size",xlab="Time")
+  lines(x=sim.res[[k]]$time,sim.res[[k]]$I, col="Red")
+  lines(x=sim.res[[k]]$time,sim.res[[k]]$R, col="Blue")
+  
+  if(k==1){title(interv[1])}
+  
+  l<-k+3
+  n<-sim.res[[l]]$S[1]+sim.res[[l]]$I[1]+sim.res[[l]]$R[1]
+  plot(sim.res[[l]]$time,sim.res[[l]]$S,type="l",col="Green",ylim=c(0,n),ylab="Population size",xlab="Time")
+  lines(x=sim.res[[l]]$time,sim.res[[l]]$I, col="Red")
+  lines(x=sim.res[[l]]$time,sim.res[[l]]$R, col="Blue")
+  lines(x=sim.res[[l]]$time,sim.res[[l]]$V, col="Black")
+
+  if(k==1){title(interv[2])}
+  
+  l<-k+6
+  n<-sim.res[[l]]$S[1]+sim.res[[l]]$I[1]+sim.res[[l]]$R[1]
+  plot(sim.res[[l]]$time,sim.res[[l]]$S,type="l",col="Green",ylim=c(0,n),ylab="Population size",xlab="Time")
+  lines(x=sim.res[[l]]$time,sim.res[[l]]$I, col="Red")
+  lines(x=sim.res[[l]]$time,sim.res[[l]]$R, col="Blue")
+  lines(x=sim.res[[l]]$time,sim.res[[l]]$V, col="Black")
+  
+  if(k==1){title(interv[3])}
+  
+  if(k==3){legend("right", c("Susceptibles", "Infecteds", "Recovereds", "Vaccinated"), 
+         pch = 1, col = c("Green","Red","Blue","Black"))}
+}
 dev.off()
 
 
---
+++ BGC Report GSE functions.R
@@ -0,0 +1,98 @@
+GSE.sim<-function(n,m,lambda,gamma,vacc.ant,vacc.start.T,vacc.int.T,seed){
+
+  set.seed(seed)
+  
+  # Create data
+  sim.data<-data.frame(inf=rep(0,n),
+                       rec=rep(0,n),
+                       inf.t=rep(NA,n),
+                       inf.length=rep(NA,n),
+                       contacts=rep(NA,n),
+                       rec.t=rep(NA,n))
+
+  # Adding index case(s)
+  index.no<-sample(x=1:n,size=m)
+  sim.data[index.no,]<-matrix(data=rep(c(1,0,0,NA,NA,NA),m),nrow=m,byrow=T)
+
+  # Counting variables
+  t.int<-vacc.start.T
+  vacc.nr<-1
+  
+  # Simulation
+  while(sum(sim.data$inf>sim.data$rec)>0){
+    # Identifies the next infectious person
+    inf.no<-which(sim.data$inf.t==min(sim.data$inf.t[sim.data$inf==1 & sim.data$rec==0],na.rm=T))
+    if(length(inf.no)==0){break}
+  
+    # Vaccination
+    if(min(sim.data[inf.no,3])>t.int && vacc.ant>0){
+      to.vacc<-((vacc.nr-1)*vacc.ant+1):(vacc.nr*vacc.ant)
+    
+      # Indicating vaccinated as "recovered"
+      sim.data[to.vacc,2]<-1
+    
+      # Infected after vaccination
+      after.vac<-sim.data[to.vacc,1]==1 & sim.data[to.vacc,3]>t.int
+      sim.data[to.vacc[after.vac],1]<-0
+      sim.data[to.vacc[after.vac],3]<-NA
+    
+      # print(paste("Vaccinated",vacc.ant,"persons at t =",t.int,sep=" "))
+      t.int<-t.int+vacc.int.T
+      vacc.nr<-vacc.nr+1
+  }
+  
+    # Identifies the next infectious person
+    inf.no<-which(sim.data$inf.t==min(sim.data$inf.t[sim.data$inf==1 & sim.data$rec==0],na.rm=T))
+    if(length(inf.no)==0){break}
+  
+    # Simulates infection time and no of contacts
+    sim.data[inf.no,4]<-rexp(length(inf.no),rate=1/gamma)
+    sim.data[inf.no,5]<-rpois(length(inf.no),lambda*sim.data[inf.no,4])
+  
+    # Categorizing the infectious persons as "recovered"
+    sim.data[inf.no,2]<-1
+  
+    # Randomizing new infectious persons
+    for(j in 1:length(inf.no)){
+      if(sim.data[inf.no[j],5]>0){
+        new.inf<-ceiling(n*runif(sim.data[inf.no[j],5]))
+        new.inf<-new.inf[sim.data[new.inf,2]==0] # Only selecting "new" infections
+      
+        # Simulating infection times for newly infected
+        if(length(new.inf)>0){
+          sim.data[new.inf,1]<-1
+        
+          inf.t<-runif(length(new.inf),
+                       min=sim.data[inf.no[j],3],
+                       max=sim.data[inf.no[j],3]+sim.data[inf.no[j],4])
+          sim.data[new.inf,3]<-pmin(sim.data[new.inf,3],inf.t,na.rm=T)
+        }
+      }  
+    }
+  }
+
+  # Calculates the recovery time
+  inf<-!is.na(sim.data$inf.t)
+  sim.data$rec.t<-sim.data$inf.t+sim.data$inf.length
+
+  
+  # Summarizing the data based on time at rec and inf
+  inf.df<-as.data.frame(aggregate(sim.data$inf.t,by=list(sim.data$inf.t),FUN=length))
+  names(inf.df)<-c("time","new.inf")
+  
+  rec.df<-as.data.frame(aggregate(sim.data$rec.t,by=list(sim.data$rec.t),FUN=length))
+  names(rec.df)<-c("time","new.rec")
+  
+  sim.data.aggr<-merge(x=inf.df,y=rec.df,by="time",all=T)
+  sim.data.aggr[is.na(sim.data.aggr)]<-0
+  
+  # Calculating S(t), I(t) and R(t)
+  sim.data.aggr$S<-n-cumsum(sim.data.aggr$new.inf)
+  sim.data.aggr$R<-cumsum(sim.data.aggr$new.rec)
+  sim.data.aggr$I<-cumsum(sim.data.aggr$new.inf)-sim.data.aggr$R
+  sim.data.aggr$V<-0
+  for(i in 1:(vacc.nr-1)){
+    sim.data.aggr$V	[sim.data.aggr$time>vacc.start.T+(i-1)*vacc.int.T]<-i*vacc.ant}
+  
+  return(list(sim.data,sim.data.aggr))
+}
